a87ae8eac7ac65c77ec3fec26be38da0c31e1cdd
Avery Wagar <awagar@cfchildren.org>
2019-07-29 15:03:17 -0700

    added blake2b-rs

--- setup.sh
+++ setup.sh
@@ -7,4 +7,7 @@ git submodule add git@github.com:glimmercoin/shimmer shimmer
 # Clone wave repo
 git submodule add git@github.com:glimmercoin/wave wave
 
+# Clone blake2b repo
+git submodule add git@github.com:glimmercoin/blake2b-rs blake2b-rs
+
 git submodule update --init --recursive
