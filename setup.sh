e023ab55d9fb825eb2e2be4e18f5fda7044f9ec2
Avery Wagar <ajmw.subs@gmail.com>
2019-07-28 23:46:35 -0700

    Setup setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+# Clone core repo
+git submodule add git@github.com:glimmercoin/core core
+
+# Clone shimmer repo
+git submodule add git@github.com:glimmercoin/shimmer shimmer
+
+# Clone wave repo
+git submodule add git@github.com:glimmercoin/wave wave
+
+git submodule update --init --recursive
